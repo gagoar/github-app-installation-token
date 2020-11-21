4117ef3022efcdde0b14f2627b598be36209dede
Gago <xeroice@gmail.com>
2020-11-20 22:13:44 -0800

    fixing type
    what about now
    
    covering console.log as well

--- src/utils/guards.ts
+++ src/utils/guards.ts
@@ -1,11 +1,11 @@
 import { AppsCreateInstallationAccessTokenResponseData } from '@octokit/types';
 
 export const isObject = (value: unknown): value is Record<string, unknown> => {
-  return value && typeof value === 'object';
+  return !!value && typeof value === 'object';
 };
 export const isString = (value: unknown): value is string => typeof value === 'string';
 export const isAppsCreateInstallationAccessTokenResponseData = (
   response: unknown
 ): response is AppsCreateInstallationAccessTokenResponseData => {
-  return isObject(response) && response.token && typeof response.token === 'string';
+  return isObject(response) && typeof response?.token === 'string';
 };
