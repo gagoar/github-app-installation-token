69814e7509a01cf22d9fd1e55e5c4daa1073ddaf
Gago <xeroice@gmail.com>
2020-12-13 23:42:50 -0800

    add rawResponse to cli

--- src/utils/guards.ts
+++ src/utils/guards.ts
@@ -1,11 +1,36 @@
-import { AppsCreateInstallationAccessTokenResponseData } from '@octokit/types';
-
+/**
+ * Default permission level members have for organization repositories:
+ * \* `read` - can pull, but not push to or administer this repository.
+ * \* `write` - can pull and push, but not administer this repository.
+ * \* `admin` - can pull, push, and administer this repository.
+ * \* `none` - no permissions granted by default.
+ */
+type DefaultRepositoryPermission = 'read' | 'write' | 'admin' | 'none';
+export interface AppsCreateInstallationAccessTokenResponse {
+  type: string;
+  tokenType: string;
+  token: string;
+  installationId: number;
+  permissions: {
+    actions: DefaultRepositoryPermission;
+    administration: DefaultRepositoryPermission;
+    checks: DefaultRepositoryPermission;
+    contents: DefaultRepositoryPermission;
+    issues: DefaultRepositoryPermission;
+    metadata: DefaultRepositoryPermission;
+    pull_requests: DefaultRepositoryPermission;
+    statuses: DefaultRepositoryPermission;
+  };
+  createdAt: string;
+  expiresAt: string;
+  repositorySelection: 'all' | 'selected';
+}
 export const isObject = (value: unknown): value is Record<string, unknown> => {
   return !!value && typeof value === 'object';
 };
 export const isString = (value: unknown): value is string => typeof value === 'string';
-export const isAppsCreateInstallationAccessTokenResponseData = (
+export const isAppsCreateInstallationAccessTokenResponse = (
   response: unknown
-): response is AppsCreateInstallationAccessTokenResponseData => {
+): response is AppsCreateInstallationAccessTokenResponse => {
   return isObject(response) && typeof response?.token === 'string';
 };
